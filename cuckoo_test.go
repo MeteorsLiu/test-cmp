72286ff0c47e6d7fe858cc48a67598f7c38f53e0
GuoHaoLan <coopticks@hotmail.com>
2022-12-04 22:12:20 +0800

    P

--- cuckoo_test.go
+++ cuckoo_test.go
@@ -8,20 +8,26 @@ import (
 
 var rng wyhash.SRNG
 
-func BenchmarkRandiGo(b *testing.B) {
+func BenchmarkRandiGoMod(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		_ = rand.Int31() % 2
 	}
 }
 
-func BenchmarkRandiWyng(b *testing.B) {
+func BenchmarkRandiWyrng(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		_ = uint32(uint64(uint32(rng.Uint64())) * uint64(2) >> 32)
 	}
 }
 
+func BenchmarkRandiWyrngMod(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = rng.Uint64() % 2
+	}
+}
 func BenchmarkMetroHash(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
